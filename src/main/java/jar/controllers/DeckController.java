20a435bdb4a2d1d5097c1c33c514b86537b4dfd3
maciejPaluszkiewicz <maciej.j.paluszkiewicz@gmail.com>
2018-10-03 11:02:17 +0200

    add DeckController class

---
+++ src/main/java/jar/controllers/DeckController.java
@@ -0,0 +1,17 @@
+package controllers;
+
+import model.Deck;
+import model.Card;
+import dao.CardDAO;
+
+public class DeckController {
+
+    public Deck loadDeck(int amountOfCards){
+        CardDAO cardDao = new CardDAO();
+        List<Card> wholeDeck = cardDao.getAll();
+        
+        Deck deck = new Deck(wholeDeck);
+        deck.setDeckSize(amountOfCards);
+        return deck;
+    }
+}
