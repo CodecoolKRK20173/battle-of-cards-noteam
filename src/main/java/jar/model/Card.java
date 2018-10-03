125d00180121fe2e3931fa051060ef8fb067c106
maciejPaluszkiewicz <maciej.j.paluszkiewicz@gmail.com>
2018-10-03 11:04:56 +0200

    add Card class

---
+++ src/main/java/jar/model/Card.java
@@ -0,0 +1,58 @@
+package model;
+
+public class Card {
+
+    private String id;
+    private String name;
+    private int strength;
+    private int toughness;
+    private int magic;
+    private int speed;
+
+    public Card(String id, String name){
+        this.id = id;
+        this.name = name;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getStrength() {
+        return strength;
+    }
+
+    public void setStrength(int strength) {
+        this.strength = strength;
+    }
+
+    public int getToughness() {
+        return toughness;
+    }
+
+    public void setToughness(int toughness) {
+        this.toughness = toughness;
+    }
+
+    public int getMagic() {
+        return magic;
+    }
+
+    public void setMagic(int magic) {
+        this.magic = magic;
+    }
+
+    public int getSpeed() {
+        return speed;
+    }
+
+    public void setSpeed(int speed) {
+        this.speed = speed;
+    }
+
+}
+
