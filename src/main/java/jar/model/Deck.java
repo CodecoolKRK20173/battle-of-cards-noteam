61d98cad8adc7e69b778141c70057d2197439043
maciejPaluszkiewicz <maciej.j.paluszkiewicz@gmail.com>
2018-10-03 11:06:36 +0200

    add Deck class

---
+++ src/main/java/jar/model/Deck.java
@@ -0,0 +1,25 @@
+package model;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Deck {
+
+    private List <Card> deck;
+
+    public Deck(List<Card> deck){
+        this.deck = deck;
+        Collections.shuffle(this.deck);
+    }
+
+    public List<Card> getDeck(){
+        return deck;
+    }
+
+    public void setDeckSize(int numOfCards){
+        if(numOfCards < deck.size()){
+            deck = deck.subList(0, numOfCards);
+        }
+    }
+}
