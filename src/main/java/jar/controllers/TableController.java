783a4acaa0bd87e75aecbd46380d8845e9feb53f
Konrad Suseł <konradsusel@gmail.com>
2018-10-04 10:09:43 +0200

    Refactor

--- src/main/java/jar/controllers/TableController.java
+++ src/main/java/jar/controllers/TableController.java
@@ -4,29 +4,21 @@ package main.java.jar.player;
 public class TableController {
     List<Card> cardsOnTable = new ArrayList<>();
 
-
-    public void round(List<Player> players, String stat) {
+    public List<Card> round(List<Player> players, String stat) {
         
         
         for (int i = 0; i < players.size(); i++) {
-            cardsOnTable.add(players[i].getNextCard());
-        }
-        
-        // compare stats here to find who won
-
-        
-    }
-
-    public getStatComparison(List<Card> cardsOnTable, String statName) { //cardsOnTable - list of cards to compare stats
-        
-        
-        //for (int i = 0; i < cardsOnTable.size() ; i++) {
-            // compare using 
-            // if (statName.equals("Strength")) {
-            //     cardsOnTable[i].getStrength()
-
-            //}
             
+            Card temp = players[i].drawCard();
+            
+            if (temp != null) {
+                cardsOnTable.add(temp);
+            } else {
+                return null;
+            }
+        
+        return cardsOnTable;
         }
+
     }
 }
